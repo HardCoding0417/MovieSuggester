fd51b86fb5879bdf8b7d1020f4bf57986112fb37
문정환 <hardluck0417@gmail.com>
2023-10-24 14:23:47 +0900

    wordcloud로 그래프를 그려봤고 TF-IDF를 적용해보았음

--- MoonJeangHwan/job05_word_cloud.py
+++ MoonJeangHwan/job05_word_cloud.py
@@ -9,13 +9,20 @@ font_name = font_manager.FontProperties(fname=font_path).get_name()
 plt.rc('font', family='NanumBarunGothic')
 
 df = pd.read_csv('../data/cleaned_one_reviews.csv')
-words = df.iloc[0, :]
+words = df.iloc[0, 1].split()
 print(words)
 
+worddict = collections.Counter(words)
+worddict = dict(worddict)
+print(worddict)
 
+wordcloud_img = WordCloud(
+    background_color='white', max_words=2000, font_path=font_path
+).generate_from_frequencies(worddict)
 
-
-
-
+plt.figure(figsize=(12,12))
+plt.imshow(wordcloud_img, interpolation='bilinear')
+plt.axis('off')
+plt.show()
 
 
